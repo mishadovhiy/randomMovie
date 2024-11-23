fa9aa2d4596b018678614c1fc3259fc52b3f5080
mishadovhiy <hi@mishadovhiy.com>
2024-11-23 06:36:42 +0200

    tabbar removed

--- MovieList/Controller/TabBarVC/TabBarVC.swift
+++ MovieList/Controller/TabBarVC/TabBarVC.swift
@@ -24,9 +24,9 @@ class TabBarVC: UITabBarController {
         self.navigationController?.setNavigationBarHidden(true, animated: false)
         DispatchQueue(label: "db", qos: .userInitiated).async {
             NetworkModel().loadAppSettings {
-                NetworkModel().openAIMovies { //movies in
-                    
-                }
+//                NetworkModel().openAIMovies { //movies in
+//                    
+//                }
                 DispatchQueue.main.async {
                     self.createSegmented()
                 }
@@ -63,17 +63,17 @@ class TabBarVC: UITabBarController {
     //add sidebar
     
     private func createSegmented() {
-        let size:CGSize = .init(width: 250, height: 40)
-        let segment = SegmentView.init(titles: [
-            .init(name: "Random", tintColor: #colorLiteral(red: 0.3449999988, green: 0.3370000124, blue: 0.8389999866, alpha: 1), selectedTextColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), deselectedColor: #colorLiteral(red: 0.3449999988, green: 0.3370000124, blue: 0.8389999866, alpha: 1)),
-            .init(name: "Movie list", tintColor: #colorLiteral(red: 0.9100000262, green: 0.9100000262, blue: 0.9100000262, alpha: 1), selectedTextColor: #colorLiteral(red: 0.08600000292, green: 0.08600000292, blue: 0.08600000292, alpha: 1), deselectedColor: #colorLiteral(red: 0.2630000114, green: 0.2630000114, blue: 0.2630000114, alpha: 1))
-        ], size: .init(width: size.width, height: size.height), background: #colorLiteral(red: 0.08600000292, green: 0.08600000292, blue: 0.08600000292, alpha: 1), selected: segmentedSelected(_:))
-        segmented = segment
-        view.addSubview(segment)
-        segment.addConstaits([.centerX:0, .top:0, .width:size.width, .height:size.height], superV: self.view, toSafe: true)
+//        let size:CGSize = .init(width: 250, height: 40)
+//        let segment = SegmentView.init(titles: [
+//            .init(name: "Random", tintColor: #colorLiteral(red: 0.3449999988, green: 0.3370000124, blue: 0.8389999866, alpha: 1), selectedTextColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), deselectedColor: #colorLiteral(red: 0.3449999988, green: 0.3370000124, blue: 0.8389999866, alpha: 1)),
+//            .init(name: "Movie list", tintColor: #colorLiteral(red: 0.9100000262, green: 0.9100000262, blue: 0.9100000262, alpha: 1), selectedTextColor: #colorLiteral(red: 0.08600000292, green: 0.08600000292, blue: 0.08600000292, alpha: 1), deselectedColor: #colorLiteral(red: 0.2630000114, green: 0.2630000114, blue: 0.2630000114, alpha: 1))
+//        ], size: .init(width: size.width, height: size.height), background: #colorLiteral(red: 0.08600000292, green: 0.08600000292, blue: 0.08600000292, alpha: 1), selected: segmentedSelected(_:))
+//        segmented = segment
+//        view.addSubview(segment)
+//        segment.addConstaits([.centerX:0, .top:0, .width:size.width, .height:size.height], superV: self.view, toSafe: true)
         
         viewControllers?.forEach({
-            $0.additionalSafeAreaInsets.top = size.height
+            $0.additionalSafeAreaInsets.top = 0
         })
         tabBar.isHidden = true
     }
